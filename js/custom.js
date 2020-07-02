79dda8c54007ba73a9e438688d06702d091f9178
Cool BRG <budhram.gurung01@gmail.com>
2020-07-02 11:31:04 +0530

    Updated with last minute changes

--- js/custom.js
+++ js/custom.js
@@ -3,5 +3,9 @@ $(function() {
     var lightbox = $("#gallery a").simpleLightbox();
 
     // Activate wow.js
-    new WOW().init();  
+    new WOW().init();
+
+    $(".navbar .nav-link").click(function () {
+      $(".navbar-collapse").collapse("hide");
+    });
 });
